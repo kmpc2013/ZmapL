071b4ef79c4955e29596a22ee19b7d9390883a54
kmpc2013 <luis.vidio9@gmail.com>
2024-04-04 16:01:27 -0300

    Ajustado arquivo init.sql

--- others/init.sql
+++ others/init.sql
@@ -2,10 +2,11 @@ CREATE DATABASE IF NOT EXISTS zmapl_prod;
 USE zmapl_prod;
 CREATE TABLE veeam (
     id INT PRIMARY KEY AUTO_INCREMENT,
-    VeeamRepoFilesystem VARCHAR(25),
+    VeeamRepoFilesystem VARCHAR(100),
+    VeeamTenant VARCHAR(100),
     VeeamAcronym VARCHAR(5),
     LigeroCustomerId VARCHAR(45) NOT NULL,
     LigeroEmail VARCHAR(45) NOT NULL,
     LigeroService VARCHAR(45) NOT NULL,
-    changedAt VARCHAR(45) NOT NULL
+    ChangedAt VARCHAR(45) NOT NULL
 )
